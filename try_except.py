8f37308d766b8774f04f059b22ecf349e3124655
nsg <dwjwlnc@sina.com.cn>
2019-03-06 19:50:18 +0800

    add try_except.py

---
+++ try_except.py
@@ -0,0 +1,15 @@
+try:
+    print('try...')
+    if 1==0:
+        raise ValueError('当然这是不可能的了哈哈哈')
+    r = 10 / int('a')
+    print('result:', r)
+except  (ValueError,TypeError) as e:
+    print('ValueError:', e)
+except ZeroDivisionError as e:
+    print('ZeroDivisionError:', e)
+else:
+    print('no error')
+finally:
+    print('finally...')
+print('END')

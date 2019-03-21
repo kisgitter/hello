64bd208f25003d6a245f1c8a2fb87c660dcf987b
nsg <dwjwlnc@sina.com.cn>
2019-03-21 20:50:57 +0800

    更新协程

--- xiecheng.py
+++ xiecheng.py
@@ -1,20 +1,20 @@
 def consumer():
     r = ''
     while True:
-        m = yield r
+        m = yield r #yield r 输出 r ,上一次执行到yield r 终止，再次开启从 m 开始，同时m的值等于produce中发送过来的n
         if not m:
             return
         print('[CONSUMER] Consuming %s...' % m)
         r = '200 OK'
 
 def produce(c):
-    #启动生成器
+    #启动生成器,也可以写作next(c),效果一样i
     c.send(None)
     n = 0
     while n < 5:
         n = n + 1
         print('[PRODUCER] Producing %s...' % n)
-        r = c.send(n)
+        r = c.send(n) #发送数据给生成器，下一步执行consumer() 中的 n = yield r
         print('[PRODUCER] Consumer return: %s' % r)
     c.close()
 
